3f65d4e9ecf7c58fde6839970cdf83c7fb5fe523
Spiral-cloud <thornalleyjack@gmail.com>
2026-02-10 10:37:55 +0000

    hopefully fixed clock

--- main.js
+++ main.js
@@ -5,5 +5,10 @@ function ShowTime()
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
+    am_pm = AM;
 }
+hour = hour < 1 ? "0" + hour : hour;
+min = min < 1 ? "0" + min : min;
+sec = sec < 1 ? "0" + sec : sec;
+let currentTime = hour + ":" + min + "" + sec + am_pm
 document.getElementById("clock").innerHTML = currentTime
